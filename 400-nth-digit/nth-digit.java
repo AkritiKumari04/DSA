74f47c9ca7b354e0896e4c10cd4caddcc15e58c5
AKRITI KUMARI <akriti.kumari.0490@gmail.com>
2026-09-21 11:16:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (40.42%) - LeetSync

---
+++ 400-nth-digit/nth-digit.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findNthDigit(int n) {
+
+        long digitLength = 1;
+        long count = 9;
+        long start = 1;
+
+      
+        while (n > digitLength * count) {
+            n -= digitLength * count;
+            digitLength++;
+            count *= 10;
+            start *= 10;
+        }
+
+        long number = start + (n - 1) / digitLength;
+
+        
+        int index = (int)((n - 1) % digitLength);
+
+        String s = String.valueOf(number);
+
+        return s.charAt(index) - '0';
+    }
+}
